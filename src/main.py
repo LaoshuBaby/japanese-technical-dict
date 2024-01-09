c0dd8ac39e62b8ce1f1aa723a71552b429ed54b4
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-09 20:20:06 +0800

    refactor: deprecate csv version dict and confirm main.py as entry file.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 import os
 import json
 
-json_file=open(os.path.join(__file__.replace("csv2json.py",""),"..","data","dict.json"),"w",encoding="utf-8")
-json_content=json.loads(json_file.read())
-json_file.close()
+def init():
+    json_file=open(os.path.join(__file__.replace("main.py",""),"..","data","dict.json"),"w",encoding="utf-8")
+    json_content=json.loads(json_file.read())
+    json_file.close()
+
+def build_pdf():
+    pass
+
+def manage_word():
+    pass
+
+def build_id():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
