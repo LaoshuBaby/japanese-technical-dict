969e9c2f2cc016b0fe04c8022bd93ae3abe90527
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-09 21:37:24 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import os
 import json
+import hashlib
 
 def init():
     json_file=open(os.path.join(__file__.replace("main.py",""),"..","data","dict.json"),"w",encoding="utf-8")
     json_content=json.loads(json_file.read())
     json_file.close()
+    return json_content
 
 def build_pdf():
     pass
@@ -13,6 +15,15 @@ def manage_word():
     pass
 
 def build_id():
+    def gen_id(string:Dict[str,str])->str:
+        hasher=hashlib.sha1()
+        hasher.update(string)
+        return hasher.hexdigest()
+    gen_id({
+        "en":"test",
+        "ja":"テスト",
+        "zh":"测试"
+    })
     pass
 
 def main():
