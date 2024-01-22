acc7aab12aad24894b1d16c69827ef0f2d7a8271
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2024-01-22 20:04:22 +0800

    feat: 完善LaTeX模板令其能够正常编译，并初写构建函数

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import json
 import hashlib
-from typing import Dict
+from typing import Dict, List
 
 
 def init():
@@ -16,17 +16,8 @@ def init():
     json_file.close()
     return json_content
 
-
-def build_pdf():
-    pass
-
-
-def manage_word():
-    pass
-
-
-def build_id():
-    def gen_id(string: Dict[str, str]) -> str:
+def gen_id():
+    def gen_id_hasher(string: Dict[str, str]) -> str:
         hasher = hashlib.sha1()
         hasher.update(
             json.dumps(
@@ -39,14 +30,43 @@ def build_id():
         id = "-".join(re.findall(".{8}", hasher.hexdigest())).upper()
         return id
 
-    this_id = gen_id({"en": "test", "ja": "テスト", "zh": "测试"})
+    this_id = gen_id_hasher({"en": "test", "ja": "テスト", "zh": "测试"})
     print(this_id)
     pass
 
+def gen_latex(json_content:dict)-> None:
+    latex_file_path=os.path.join(os.path.realpath(__file__),"template","main.tex")
+    latex_file=open(latex_file_path,"r",encoding="utf-8")
+    latex_file_content=latex_file.read()
+    latex_file.close()
+    symbol="Word & 单词 & タンゴ \\\\ \midrule\n"
 
-def main():
+    def gen_insert_list(json_content:dict)->List[str]:
+        dict_entries=json_content["dict"]
+        insert_list=[]
+        template="{{en}} & {{zh}} & {{ja}} \\\\ \midrule"
+        for entry in dict_entries:
+            insert_list.append(template.format(**entry["string"]))
+        return insert_list
+
+    latex_file=open(latex_file_path,"w",encoding="utf-8")
+    latex_file_content=latex_file.write(latex_file_content.replace(symbol,symbol+"".join([i+" \n" for i in gen_insert_list()])))
+    latex_file.close()
+    return None
+
+def manage_word():
     pass
+
+def build_pdf()->None:
+    return None
+
+def main()->None:
+    return None
 
 
 if __name__ == "__main__":
-    build_id()
+    json_content=init()
+    # # gen_id then overwrite disk file
+    # gen_id()
+    gen_latex(json_content)
+    build_pdf()
