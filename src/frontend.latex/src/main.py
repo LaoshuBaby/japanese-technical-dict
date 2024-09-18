ccc165965606e431c5a84fe554f01a3f891a8ffe
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-09-18 18:27:45 +0800

    fix: broken path and build script

--- src/frontend.latex/src/main.py
+++ src/frontend.latex/src/main.py
@@ -3,12 +3,13 @@ import json
 import hashlib
 from typing import Dict, List
 
-MANUAL_COMPLIE=False
+MANUAL_COMPLIE = False
+
 
 def init():
     json_file = open(
         os.path.join(
-            __file__.replace("main.py", ""), "..", "data", "dict.json"
+            os.path.dirname(__file__), "..", "..", "..", "data", "dict.json"
         ),
         "r",
         encoding="utf-8",
@@ -16,6 +17,7 @@ def init():
     json_content = json.loads(json_file.read())
     json_file.close()
     return json_content
+
 
 def gen_id():
     def gen_id_hasher(string: Dict[str, str]) -> str:
@@ -35,44 +37,57 @@ def gen_id():
     print(this_id)
     pass
 
-def gen_latex(json_content:dict)-> None:
-    latex_file_path=os.path.join(os.path.dirname(__file__),"template","main.tex")
-    latex_file=open(latex_file_path,"r",encoding="utf-8")
-    latex_file_content=latex_file.read()
-    latex_file.close()
-    symbol="Word & 单词 & タンゴ \\\\ \midrule\n"
 
-    def gen_insert_list(json_content:dict)->List[str]:
-        dict_entries=json_content["dict"]
-        insert_list=[]
-        template="{en} & {zh} & {ja} \\\\ \midrule"
+def gen_latex(json_content: dict) -> None:
+    latex_file_path = os.path.join(
+        os.path.dirname(__file__), "..","template", "main.tex"
+    )
+    latex_file = open(latex_file_path, "r", encoding="utf-8")
+    latex_file_content = latex_file.read()
+    latex_file.close()
+    symbol = "Word & 单词 & タンゴ \\\\ \midrule\n"
+
+    def gen_insert_list(json_content: dict) -> List[str]:
+        dict_entries = json_content["dict"]
+        insert_list = []
+        template = "{en} & {zh} & {ja} \\\\ \midrule"
         for entry in dict_entries:
             insert_list.append(template.format(**entry["string"]))
         return insert_list
 
-    latex_file=open(latex_file_path,"w",encoding="utf-8")
-    latex_file_content=latex_file.write(latex_file_content.replace(symbol,symbol+"".join([i+" \n" for i in gen_insert_list(json_content)])))
+    latex_file = open(latex_file_path, "w", encoding="utf-8")
+    latex_file_content = latex_file.write(
+        latex_file_content.replace(
+            symbol,
+            symbol
+            + "".join([i + " \n" for i in gen_insert_list(json_content)]),
+        )
+    )
     latex_file.close()
     return None
+
 
 def manage_word():
     pass
 
-def build_pdf()->None:
+
+def build_pdf() -> None:
+    os.chdir("..")
     os.chdir("template")
     os.system("xelatex main")
     return None
 
-def main()->None:
+
+def main() -> None:
     return None
 
 
 if __name__ == "__main__":
-    json_content=init()
+    json_content = init()
     # # gen_id then overwrite disk file
     # gen_id()
     gen_latex(json_content)
-    if MANUAL_COMPLIE==False:
+    if MANUAL_COMPLIE == False:
         build_pdf()
     else:
         pass
